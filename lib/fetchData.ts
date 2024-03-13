2d94eb5384b3ac31e73cb1671ba3c77df0f4ef4b
墨綠BG <s5460703@gmail.com>
2024-03-14 01:06:20 +0800

    🔧 chore：删除多余依赖

--- lib/fetchData.ts
+++ lib/fetchData.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { CapacitorHttp } from "@capacitor/core";
 
 const preurl = "https://zhijing.bigdust.space/api/v1";
@@ -25,48 +24,48 @@ export async function postData(url = "", data = {}) {
 	}
 }
 
-export async function getJson(url = "") {
-	try {
-		// if (!token) {
-		// 	return;
-		// }
+// export async function getJson(url = "") {
+// 	try {
+// 		// if (!token) {
+// 		// 	return;
+// 		// }
 
-		const response = await axios({
-			method: "GET",
-			url: preurl + url,
-			headers: {
-				"Content-Type": "application/json;charset=utf-8",
-				// "Authorization": token,
-			},
-		});
+// 		const response = await axios({
+// 			method: "GET",
+// 			url: preurl + url,
+// 			headers: {
+// 				"Content-Type": "application/json;charset=utf-8",
+// 				// "Authorization": token,
+// 			},
+// 		});
 
-		return response.data;
-	} catch (error) {
-		console.error(error);
-	}
-}
+// 		return response.data;
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// }
 
-export async function putData(url = "", data = {}, method = "PUT") {
-	try {
-		// if (!token) {
-		// 	return;
-		// }
+// export async function putData(url = "", data = {}, method = "PUT") {
+// 	try {
+// 		// if (!token) {
+// 		// 	return;
+// 		// }
 
-		const response = await axios({
-			method: method,
-			url: preurl + url,
-			headers: {
-				"Content-Type": "application/json;charset=utf-8",
-				// "Authorization": token,
-			},
-			data: data,
-		});
+// 		const response = await axios({
+// 			method: method,
+// 			url: preurl + url,
+// 			headers: {
+// 				"Content-Type": "application/json;charset=utf-8",
+// 				// "Authorization": token,
+// 			},
+// 			data: data,
+// 		});
 
-		return response.data;
-	} catch (error) {
-		console.error(error);
-	}
-}
+// 		return response.data;
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// }
 
 export async function authPost(url = "", data = {}) {
 	try {
