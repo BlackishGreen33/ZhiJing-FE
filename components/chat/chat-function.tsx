eed428fb4f9740d648fa09fa0d15c0f90a8691c1
墨綠BG <s5460703@gmail.com>
2024-03-23 21:00:46 +0800

    ✨ feat：新增生成教案组件

--- components/chat/chat-function.tsx
+++ components/chat/chat-function.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import SimilarQuestions from "@/components/function-button/similar-questions";
+import PlanGeneration from "../function-button/plan-generation";
 
 type ChatFuctionProps = {
 	userType: "student" | "teacher";
@@ -11,6 +12,7 @@ const ChatFunction: React.FC<ChatFuctionProps> = ({ userType }) => {
 		<div className="w-full h-[5vh] fixed bottom-[15.5vh] flex flex-col">
 			<div className="w-full h-[0.1vh] bg-gray-700"></div>
 			{userType === "student" && <SimilarQuestions />}
+			{userType === "teacher" && <PlanGeneration />}
 		</div>
 	);
 };
---
+++ components/function-button/plan-generation.tsx
@@ -0,0 +1,74 @@
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const PlanGeneration = () => {
+	return (
+		<div className="w-full h-[4.5vh] flex items-center">
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button
+						className="absolute border-0 w-[20%] h-[3vh] p-0 left-[3%]"
+						variant="default"
+						size="sm"
+					>
+						教案生成
+					</Button>
+				</DialogTrigger>
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>教案生成</DialogTitle>
+						<DialogDescription>请填写以下信息</DialogDescription>
+					</DialogHeader>
+					<div className="grid gap-4 py-4">
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="text" className="text-right">
+								教学内容
+							</Label>
+							<Textarea
+								id="text"
+								placeholder="请输入教学内容"
+								className="col-span-3"
+							/>
+						</div>
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="text" className="text-right">
+								学生年级
+							</Label>
+							<Input
+								id="text"
+								placeholder="请输入学生年级"
+								className="col-span-3"
+							/>
+						</div>
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="text" className="text-right">
+								课程类型
+							</Label>
+							<Input
+								id="text"
+								placeholder="请输入课程类型"
+								className="col-span-3"
+							/>
+						</div>
+					</div>
+					<DialogFooter>
+						<Button type="submit">生成教案</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</div>
+	);
+};
+
+export default PlanGeneration;
